19664d0425b865a9f531be287640e33aba636d55
Thanos Tsianis <nektariosts@gmail.com>
2020-12-30 00:59:56 +0200

    add explanatory comment on closures, and useRef() hook

--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -10,11 +10,24 @@ const Search = React.memo((props) => {
   const { onLoadIngredients } = props;
 
   const [enteredFilter, setEnteredFilter] = useState("");
-
+  // the useRef hook allows us to create references on any element we want
   const inputRef = useRef();
 
   useEffect(() => {
     setTimeout(() => {
+      // We need to undrstand how CLOSURES work 
+      // in order to understand how the following line makes sense
+      // 'enteredFilter' here is not going to be updated on every key stroke. 
+      // The 'setTimeout' method ENCLOSES in it
+      // the value that 'enteredFilter' HAD AT THE EXACT TIME  THAT THE FUNCTION WAS CALLED
+      // That's why we need a way to compare this value with the current value
+
+      // So in a nutshell we say: 
+      // IF the value of the input when the function started executing 
+      // is the same with the value of the input now (500 miliseconds later)
+      // (basically if the user has paused typing for 0.5 seconds) THEN send the request !
+      // Open the Network tab on the browser while this runs to see that
+      // a request is only being sent when a typing pause happens
       if (enteredFilter === inputRef.current.value) {
         const query =
           enteredFilter.length === 0
