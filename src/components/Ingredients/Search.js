7b0cca1298b250981e0bb5c03415fe73f912c565
Thanos Tsianis <nektariosts@gmail.com>
2020-12-30 00:51:01 +0200

    fetch filtered data

--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,15 +1,56 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from "react";
 
-import Card from '../UI/Card';
-import './Search.css';
+import Card from "../UI/Card";
+import "./Search.css";
 
-const Search = React.memo(props => {
+const Search = React.memo((props) => {
+  // Object Destructuring
+  // We store the function in a variable with the same name so that we can declare it
+  // in the dependencies of out useEffect function
+  const { onLoadIngredients } = props;
+
+  const [enteredFilter, setEnteredFilter] = useState("");
+
+  const inputRef = useRef();
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (enteredFilter === inputRef.current.value) {
+        const query =
+          enteredFilter.length === 0
+            ? ""
+            : `?orderBy="title"&equalTo="${enteredFilter}"`;
+        fetch(
+          "https://reacthooksdemo-4f235-default-rtdb.firebaseio.com/ingredients.json" +
+            query
+        )
+          .then((response) => response.json())
+          .then((responseData) => {
+            const loadedIngredients = [];
+            for (const key in responseData) {
+              loadedIngredients.push({
+                id: key,
+                title: responseData[key].title,
+                amount: responseData[key].amount,
+              });
+            }
+            props.onLoadIngredients(loadedIngredients);
+          });
+      }
+    }, 500);
+  }, [enteredFilter, onLoadIngredients]);
+
   return (
     <section className="search">
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" />
+          <input
+            ref={inputRef}
+            type="text"
+            value={enteredFilter}
+            onChange={(event) => setEnteredFilter(event.target.value)}
+          />
         </div>
       </Card>
     </section>
