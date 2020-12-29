6c59ee761c6bf168fd63d8176d059f3daae5064d
Thanos Tsianis <nektariosts@gmail.com>
2020-12-29 19:01:27 +0200

    add ingrefients to Firebase too

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,24 +1,42 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
-import IngredientForm from './IngredientForm';
-import IngredientList from './IngredientList';
-import Search from './Search';
+import IngredientForm from "./IngredientForm";
+import IngredientList from "./IngredientList";
+import Search from "./Search";
 
 function Ingredients() {
+  const [ingredients, setIngredients] = useState([]);
 
-  const [ingredients, setIngredients ] = useState([]);
-
-  const addIngredientHandler = ingredient => {
-    setIngredients(previousIngredients => 
-      [...previousIngredients, {id: Math.random().toString(), ...ingredient}]
-    );
+  const addIngredientHandler = (ingredient) => {
+    fetch(
+      "https://reacthooksdemo-4f235-default-rtdb.firebaseio.com/ingredients.json",
+      {
+        method: "POST",
+        body: JSON.stringify({ ingredient }),
+        headers: { "Content-Type": "application/json" },
+      }
+    )
+      .then((response) => {
+        console.log(response);
+        // response.json return a Promise that contains the body of the response !!!
+        return response.json();
+      })
+      .then((responseData) => {
+        console.log(responseData);
+        // Firebase automnatically gives every posted element a key, that is store
+        // under the name property !
+        setIngredients((previousIngredients) => [
+          ...previousIngredients,
+          { id: responseData.name, ...ingredient },
+        ]);
+      });
   };
 
   const removeIngredientHandler = (id) => {
-    setIngredients(previousIngredients => 
-      previousIngredients.filter( (ingredient,__) => ingredient.id !== id)
-    )
-  }
+    setIngredients((previousIngredients) =>
+      previousIngredients.filter((ingredient, __) => ingredient.id !== id)
+    );
+  };
 
   return (
     <div className="App">
@@ -26,7 +44,12 @@ function Ingredients() {
 
       <section>
         <Search />
-        <IngredientList ingredients={ingredients} onRemoveItem={ (id)=>{removeIngredientHandler(id)} }/>
+        <IngredientList
+          ingredients={ingredients}
+          onRemoveItem={(id) => {
+            removeIngredientHandler(id);
+          }}
+        />
       </section>
     </div>
   );
