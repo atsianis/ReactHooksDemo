46ab2857e027b094cb6eec44e03503926afcf319
Thanos Tsianis <nektariosts@gmail.com>
2020-12-29 19:55:00 +0200

    use useEffect hook to render ingredients on page render

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import IngredientForm from "./IngredientForm";
 import IngredientList from "./IngredientList";
@@ -7,12 +7,28 @@ import Search from "./Search";
 function Ingredients() {
   const [ingredients, setIngredients] = useState([]);
 
+  useEffect( ()=> {
+    fetch('https://reacthooksdemo-4f235-default-rtdb.firebaseio.com/ingredients.json')
+      .then( response => response.json())
+      .then( responseData => {
+        const loadedIngredients = [];
+        for ( const key in responseData ){
+          loadedIngredients.push({
+            id: key,
+            title : responseData[key].title,
+            amount: responseData[key].amount
+          });
+        }
+        setIngredients(loadedIngredients);
+      })
+  }, []);
+
   const addIngredientHandler = (ingredient) => {
     fetch(
       "https://reacthooksdemo-4f235-default-rtdb.firebaseio.com/ingredients.json",
       {
         method: "POST",
-        body: JSON.stringify({ ingredient }),
+        body: JSON.stringify({ ...ingredient }),
         headers: { "Content-Type": "application/json" },
       }
     )
