a61125539a4761f04594dc318f43f71062fee46a
Thanos Tsianis <nektariosts@gmail.com>
2020-12-29 03:36:39 +0200

    introducing useState

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import Card from '../UI/Card';
 import './IngredientForm.css';
 
 const IngredientForm = React.memo(props => {
+
+  const inputState = useState({title: '', amount: ''});
+
   const submitHandler = event => {
     event.preventDefault();
     // ...
@@ -15,11 +18,28 @@ const IngredientForm = React.memo(props => {
         <form onSubmit={submitHandler}>
           <div className="form-control">
             <label htmlFor="title">Name</label>
-            <input type="text" id="title" />
+            <input type="text" id="title" 
+              value = {inputState[0].title} 
+              onChange={event => {
+                // we store the event.target.value before we enter the closure
+                // because of the way React events (synthetic event re pulling) works
+                const titleValue = event.target.value;
+                ///////////////////////////////////////////////
+                inputState[1]( prevInputState =>({
+                  title: titleValue, 
+                  amount: prevInputState.amount
+                }))}}/>
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" />
+            <input type="number" id="amount" 
+              value = {inputState[0].amount} 
+              onChange={event => {
+                const amountValue = event.target.value;
+                inputState[1]( prevInputState=>({
+                  amount: amountValue,
+                  title:prevInputState.title
+                }))}}/>
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>
