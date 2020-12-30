e3b21a60afaf53e7867896df3b69d84d50a7a5f4
Thanos Tsianis <nektariosts@gmail.com>
2020-12-30 03:47:03 +0200

    add loading and error state, trigger modal

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,17 +1,37 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect, useCallback, useReducer } from "react";
 
 import IngredientForm from "./IngredientForm";
 import IngredientList from "./IngredientList";
 import Search from "./Search";
+import ErrorModal from '../UI/ErrorModal';
+
+const ingredientReducer = ( currentIngredients, action ) => {
+  switch (action.type){
+    case 'SET':
+      return action.payload
+    case 'ADD':
+      return [...ingredients, action.payload]
+    case 'DELETE':
+      return currentIngredients.filter( ing=> ing.id !== action.payload)
+    default:
+      throw new Error('Should not get here');
+  }
+}
 
 function Ingredients() {
+
+  const [userIngredients, dispatch] = useReducer(ingredientReducer, []);
+
   const [ingredients, setIngredients] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
 
   const filteredIngredientsHandler = useCallback( filteredIngredients => 
     setIngredients(filteredIngredients)
   , []);
 
   const addIngredientHandler = (ingredient) => {
+    setIsLoading(true);
     fetch(
       "https://reacthooksdemo-4f235-default-rtdb.firebaseio.com/ingredients.json",
       {
@@ -21,6 +41,7 @@ function Ingredients() {
       }
     )
       .then((response) => {
+        setIsLoading(false);
         console.log(response);
         // response.json return a Promise that contains the body of the response !!!
         return response.json();
@@ -36,15 +57,35 @@ function Ingredients() {
       });
   };
 
+  const clearError =()=> {
+    setError(null);
+  }
+
   const removeIngredientHandler = (id) => {
-    setIngredients((previousIngredients) =>
+    setIsLoading(true);
+    fetch(
+      `https://reacthooksdemo-4f235-default-rtdb.firebaseio.com/ingredients/${id}.json`,
+      {
+        method: "DELETE",
+      }
+    ).then( response => {
+      setIsLoading(false);
+      setIngredients((previousIngredients) =>
       previousIngredients.filter((ingredient, __) => ingredient.id !== id)
-    );
+    )}
+    ).catch (error => {
+      // Check React's State Batching concept
+      setError(error.message);
+      setIsLoading(false);
+    })
+    
   };
 
   return (
     <div className="App">
-      <IngredientForm onAddIngredient={addIngredientHandler} />
+      <IngredientForm onAddIngredient={addIngredientHandler} loading={isLoading}/>
+
+      {error && <ErrorModal onClose={clearError}>{error}</ErrorModal>}
 
       <section>
         <Search onLoadIngredients={filteredIngredientsHandler}/>
